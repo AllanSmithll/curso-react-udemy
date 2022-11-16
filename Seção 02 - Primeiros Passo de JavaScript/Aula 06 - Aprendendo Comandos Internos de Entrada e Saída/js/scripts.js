1eef027bb46349e40e2f2946bcd9b2dee74d8e3d
AllanSmithll <allanammancio2004@gmail.com>
2022-11-16 09:34:28 -0300

    Aula 28 do Curso de React

---
+++ Seção 02 - Primeiros Passo de JavaScript/Aula 06 - Aprendendo Comandos Internos de Entrada e Saída/js/scripts.js
@@ -0,0 +1,18 @@
+// 12/11/2022
+// Aula de comando de estrada e saída no JavaScript
+
+// var idade = prompt("Digite sua idade: "); // Perguntando a idade
+
+document.write(`<h1>Escrevendo um <i>h1</i> com "document"</h1>`); // Aqui eu escrevo um h1 na página HTML
+document.write("<br>")  // Quebra de linha
+
+document.write("<img src='https://files.tecnoblog.net/wp-content/uploads/2021/09/logotipo_da_empresa_google.jpg'  />")
+
+document.write("<strong> Aprendendo JavaScript </strong>")  // Negrito
+
+// Exibe um "Like" <button>
+return (
+    <button onClick={() => this.setState({ liked: true })}>
+      Like
+    </button>
+  );
--- Seção 2/Aula 06/js/scripts.js
+++
@@ -1,6 +0,0 @@
-// 12/11/2022
-// Aula de comando de estrada e saída no JavaScript
-
-// var idade = prompt("Digite sua idade: "); // Perguntando a idade
-
-document.write(`<h1>Escrevendo um <i>h1</i> com "document"</h1>`); // Aqui eu escrevo um h1 na página HTML
