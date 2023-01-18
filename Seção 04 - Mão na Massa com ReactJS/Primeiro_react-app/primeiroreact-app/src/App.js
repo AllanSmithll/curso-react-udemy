2e5094b8d9d7eb7b92c98c1a877af88dd4d70007
allansmithll <allanammancio2004@gmail.com>
2023-01-17 21:55:00 -0300

    Atualizações

--- Seção 04 - Mão na Massa com ReactJS/Primeiro_react-app/primeiroreact-app/src/App.js
+++ Seção 04 - Mão na Massa com ReactJS/Primeiro_react-app/primeiroreact-app/src/App.js
@@ -1,13 +1,49 @@
-// 13/01/2022
-// Primeiro projeto React
-import React from 'react'
+import React, { Component } from 'react';
 
-function App() {
-    return (
-        <div>
-            <h1>Conheça nossa equipe:</h1>
-        </div>
-    )
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: "Allan",
+            contador: 0
+        };
+        this.aumentar = this.aumentar.bind(this)
+        this.diminuir = this.diminuir.bind(this)
+    }
+
+    aumentar() {
+        let state = this.state;
+        state.contador += 1
+        state.nome = "Anderson"
+        this.setState(state);
+    }
+
+    diminuir () {
+        let stateDiminuir = this.state;
+        if(this.diminuir === 0) {
+            alert('Chegamos a zero!');
+            return;
+        }
+
+        stateDiminuir.contador -= 1;
+        this.setState(stateDiminuir);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Contador</h1>
+                <h2>{this.state.nome}</h2>
+                <h3>
+                    <button onClick={this.diminuir}>-</button>
+                    {this.state.contador}
+                    <button onClick={this.aumentar}>+</button>
+                </h3>
+            </div>
+        )
+    }
 }
 
 export default App;
