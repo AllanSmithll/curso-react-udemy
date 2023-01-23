e10b918d72f2e6f82823e7bf9bea49948d8d8012
allansmithll <allanammancio2004@gmail.com>
2023-01-23 15:35:35 -0300

    Criando um docs

--- Seção 04 - Mão na Massa com ReactJS/Primeiro_react-app/primeiroreact-app/src/App.js
+++ Seção 04 - Mão na Massa com ReactJS/Primeiro_react-app/primeiroreact-app/src/App.js
@@ -1,49 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            nome: "Allan",
-            contador: 0
-        };
-        this.aumentar = this.aumentar.bind(this)
-        this.diminuir = this.diminuir.bind(this)
-    }
-
-    aumentar() {
-        let state = this.state;
-        state.contador += 1
-        state.nome = "Anderson"
-        this.setState(state);
-    }
-
-    diminuir () {
-        let stateDiminuir = this.state;
-        if(this.diminuir === 0) {
-            alert('Chegamos a zero!');
-            return;
-        }
-
-        stateDiminuir.contador -= 1;
-        this.setState(stateDiminuir);
-    }
-
-
-    render() {
-        return (
-            <div>
-                <h1>Contador</h1>
-                <h2>{this.state.nome}</h2>
-                <h3>
-                    <button onClick={this.diminuir}>-</button>
-                    {this.state.contador}
-                    <button onClick={this.aumentar}>+</button>
-                </h3>
-            </div>
-        )
-    }
+const Presentation = (props) => {
+    return(
+        <div>
+            <h2>Olá! Eu sou {props.nome} e tenho {props.idade} anos.</h2>
+        </div>
+    )
 }
 
-export default App;
+export default function App() {
+    // A função App que retornará nosso Aplicativo
+    return(
+        <div>
+            <h1>Olá Mundo</h1>
+            <Presentation nome="Allan" idade="18"/>
+            <Presentation nome="Anderson" idade="19" />
+        </div>
+    )
+}
+
+// import React, { Component } from 'react';
+
+// class App extends Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             nome: "Allan",
+//             contador: 0
+//         };
+//         this.aumentar = this.aumentar.bind(this)
+//         this.diminuir = this.diminuir.bind(this)
+//     }
+
+//     aumentar() {
+//         let state = this.state;
+//         state.contador += 1
+//         state.nome = "Anderson"
+//         this.setState(state);
+//     }
+
+//     diminuir () {
+//         let stateDiminuir = this.state;
+//         if(this.diminuir === 0) {
+//             alert('Chegamos a zero!');
+//             return;
+//         }
+
+//         stateDiminuir.contador -= 1;
+//         this.setState(stateDiminuir);
+//     }
+
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Contador</h1>
+//                 <h2>{this.state.nome}</h2>
+//                 <h3>
+//                     <button onClick={this.diminuir}>-</button>
+//                     {this.state.contador}
+//                     <button onClick={this.aumentar}>+</button>
+//                 </h3>
+//             </div>
+//         )
+//     }
+// }
+
+// export default App;
