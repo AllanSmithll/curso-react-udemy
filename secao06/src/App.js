dd1b4b8e27064d29ab4555eb27181977a3047439
allansmithll <allanammancio2004@gmail.com>
2023-08-03 09:59:15 -0300

    Api useState

--- secao06/src/App.js
+++ secao06/src/App.js
@@ -1,20 +1,36 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
 
-  const [taferas, setTarefas] = useState([
+  const [tarefas, setTarefas] = useState([
   'Pagar a conta de luz',
   'Estudar React Hooks'
 ]);
 
+const [input, setInput] = useState([' '])
+
+  function handleAdd() {
+    setTarefas([...tarefas, input])
+    setInput('')
+  }
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+  }, [tarefas])
+
   return(
     <div>
       <ul>
-        {setTarefas.map(tarefa => (
-          <li key={}> </li>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
         ))}
       </ul>
+      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+      <button type='button' onClick={handleAdd}>
+        Adicionar tarefa
+      </button>
+
     </div>
   );
 }
