cbc5e0b23abd0be3508a6e1281ef65d6bfeb9cfc
allansmithll <allanammancio2004@gmail.com>
2023-08-11 22:56:30 -0300

    Término de Hooks

--- secao06/src/App.js
+++ secao06/src/App.js
@@ -1,23 +1,28 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useMemo, useCallback} from 'react'
 import './App.css'
 
 function App() {
 
-  const [tarefas, setTarefas] = useState([
-  'Pagar a conta de luz',
-  'Estudar React Hooks'
-]);
+  const [tarefas, setTarefas] = useState([]);
+  const [input, setInput] = useState('');
 
-const [input, setInput] = useState([' '])
-
-  function handleAdd() {
-    setTarefas([...tarefas, input])
-    setInput('')
-  }
+  useEffect(() => { 
+    const tarefasStorage = localStorage.getItem('tarefas');
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
 
   useEffect(() => {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
   }, [tarefas])
+
+  const handleAdd = useCallback(() => {
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }, [input, tarefas]);
+
+  const totalTarefas = useMemo(() => tarefas.length, [tarefas]);
 
   return(
     <div>
@@ -26,6 +31,9 @@ const [input, setInput] = useState([' '])
           <li key={tarefa}>{tarefa}</li>
         ))}
       </ul>
+      <br/>
+      <strong>Voce tem {totalTarefas} tarefas!</strong>
+      <br/>
       <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
       <button type='button' onClick={handleAdd}>
         Adicionar tarefa
