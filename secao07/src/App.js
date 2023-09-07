9ca3791957324d23b04f18cb480ac87c32d41d89
allansmithll <allanammancio2004@gmail.com>
2023-09-07 17:06:29 -0300

    Fazendo uma aplicação na seção 7

--- secao07/src/App.js
+++ secao07/src/App.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from 'react';
+import './style.css';
+
+// https://sujeitoprogramador.com/rn-api/?api=posts
+
 function App() {
+  const [nutri, setNutri] = useState([])
+
+  useEffect(() => {
+    function loadApi() {
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+            console.log(json)
+            setNutri(json)
+          }
+        )
+    }
+    loadApi();
+  }, [])
+
   return (
-    <div className="App">
-      <h1>TESTE</h1>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+      {nutri.map((item) => {
+        return (
+          <article key={item.id} className='post'>
+            <strong className='titulo'>{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className='capa'/>
+            <p className='subtitulo'>
+                {item.subtitulo}
+              </p>
+            <a href='#' className='botao'>Acessar</a>
+          </article>
+        )
+      })}
     </div>
   );
 }
